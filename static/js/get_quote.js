d5738ba7c137935617e4d20ac7e377e6cf916f4e
Ruslan Kurbanov <wkurbanov@gmail.com>
2017-01-24 22:15:47 +0300

    Move quotes to quotes.json, change script to load random object from json file

--- static/js/get_quote.js
+++ static/js/get_quote.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-    var quotes = JSON.parse($('#quotes_json').html());
-    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    $("p.phrase").text(randomQuote['phrase']);
-    if ('signature' in randomQuote) {
-        $("p.signature").text(randomQuote['signature']);
-    }
+    $.getJSON('static/js/quotes.json', function (data) {
+        var randomQuote = data[Math.floor(Math.random() * data.length)];
+        $("p.phrase").text(randomQuote['phrase']);
+        if ('signature' in randomQuote) {
+            $("p.signature").text(randomQuote['signature']);
+        }
+    });
 });
